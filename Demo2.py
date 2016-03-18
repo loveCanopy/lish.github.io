583547a6a85942f10432e5717f59daaca7924102
loveCanopy <1039431583@qq.com>
2016-03-18 12:54:35 +0800

    sqlalchemy及表格案例

--- Demo2.py
+++ Demo2.py
@@ -1,66 +1,59 @@
-__author__ = 'Sherlock'
+from sqlalchemy import Table,Column,Integer,String,MetaData,ForeignKey
+from sqlalchemy import create_engine #数据库类型
+meta=MetaData()
+#创建表users
+users = Table('users', meta,
+Column('id', Integer, primary_key=True),
+Column('name', String(10)),
+Column('fullname', String(10)),)
+#创建表addresses
+addresses = Table('addresses', meta,
+Column('id', Integer, primary_key=True),
+Column('user_id', None, ForeignKey('users.id')),
+Column('email_address', String(20), nullable=False)
+)
+#连接数据库
+engine=create_engine("mysql+pymysql://root:root@localhost/contact?charset=utf8",echo=True) #mysql+pymysql://<username>:<password>@<host>/<dbname>[?<options>]
+#表加入数据库中
+meta.create_all(engine)
 
-import urllib.request
-import os
-import random
+#插入
+# ins = users.insert().values(name='jack', fullname='Jack Jones')
+con=engine.connect() #创建数据库连接
+# con.execute(ins) #执行操作
 
-def url_open(url):
+# ins1=users.insert()
+# con.execute(ins1, id=3, name='wendy', fullname='Wendy')
+#批量插入
+# con.execute(addresses.insert(),[
+# {'user_id': 1, 'email_address' : 'jack@yahoo'},
+# {'user_id': 1, 'email_address' : 'jack@msn'},
+# {'user_id': 2, 'email_address' : 'www@www'},
+# {'user_id': 2, 'email_address' : 'wendy@aol'},
+#  ])
 
-    req = urllib.request.Request(url)
+#选择
+from sqlalchemy import select
+s=select([users])
+result=con.execute(s)
+# for i in result:
+#
+# result.close()
 
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 5.1; rv:43.0) Gecko/20100101 Firefox/43.0')
+s1=select([users,addresses]).where(users.c.id==addresses.c.user_id)
+s2=select([users]).where(users.c.id==1)
+result=con.execute(s2)
+for i in result:
+    print(i)
 
-
-    response = urllib.request.urlopen(req)
-    html = response.read()
-
-    return html
-
-def get_page(url):
-    html = url_open(url).decode('utf-8')
-
-    a = html.find('current-comment-page') + 23
-    b = html.find(']', a)
-
-    return html[a:b]
-def find_imgs(url):
-    html = url_open(url).decode('utf-8')
-    img_addrs = []
-
-    a = html.find('img src=')
-
-    while a != -1:
-        b = html.find('.jpg', a, a+255)
-
-        if b != -1:
-            img_addrs.append(html[a+9:b+4])
-        else:
-            b = a + 9
-
-        a = html.find('img src=', b)
-
-    return img_addrs
-
-def save_imgs(folder, img_addrs):
-    for each in img_addrs:
-        filename = each.split('/')[-1]
-        with open(filename, 'wb') as f:
-            img = url_open(each)
-            f.write(img)
-
-def download_mm(folder='OOXX', pages=10):
-    os.mkdir(folder)
-    os.chdir(folder)
-
-    url = 'http://jandan.net/ooxx/'
-    page_num = int(get_page(url))
-
-    for i in range(pages):
-        page_num -= i
-        page_url = url + 'page-' + str(page_num) + '#comments'
-        img_addrs = find_imgs(page_url)
-        save_imgs(folder, img_addrs)
+#删除
+# con.execute(addresses.delete())
+# con.execute(users.delete().where(users.c.name > 'm'))
+#更新
+stmt = users.update().\
+        values(name='hahahhah').\
+        where(users.c.id == 1)
+con.execute(stmt)
 
 
-if __name__ == '__main__':
-    download_mm()
+
--- Demo3.py
+++ Demo3.py
@@ -1,48 +1,75 @@
-__author__ = 'Sherlock'
-import urllib.request as u
-import os
+from __future__ import print_function
+#IP转数字
+def ip2int(ip):
+    try:
+        hexn=''.join(["%02X" %int(i) for i in ip.split('.')])
+    except Exception:
+        hexn=''.join(["%02X" %int(i) for i in '0.0.0.0'.split('.')])
+    return int(hexn,16)
 
-def url_open(url):
-    req = u.Request(url)
-    req.add_header('User-Agent' , 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36 SE 2.X MetaSr 1.0')
-    response = u.urlopen(url)
-    html = response.read()
-    return html
+with open('ipdata.csv','rb') as f:
+    info=f.readlines()
+    c=[]
+    for line in info:
+        line=line.decode(encoding='utf-8')
+        sinfo=line.strip().split(',',4)
+        c1,c2,c3,c4,c5=sinfo[0],ip2int(sinfo[1]),ip2int(sinfo[2]),sinfo[3],sinfo[4]
+        cl1=(c2,c3,c4,c5)
+        c.append(cl1)
+# print(c[:10])
+import pymysql
+con=pymysql.connect(host="localhost",user="root",passwd="root",db="contact",charset='utf8')
+cur=con.cursor()
+#
+# # for i in c[0]:
+# #     print(c[0])
+# #     ret=cur.execute("insert into ipdata values (%s,%s,%s,%s,%s)",i)
+# cur.execute("insert into ipdata values (null,%s,%s,%s,%s)",c[0])
+# for i in range(len(c)//1000+1):
+#     tempc=c[i*1000:(i+1)*1000]   #每次插入1000条 防止数据库崩溃
+#     ret=cur.executemany("insert into ipdata values(null,%s,%s,%s,%s)",tempc)
+# cur.execute("commit")
+# con.commit()
+# for i in range(10):
+#     tempc=c[i]
+#     ret=cur.executemany("insert into ipdata values(NULL ,%s,%s,%s,%s)",tempc)
+# cur.execute("commit")
+#数字转IP
+def int2ip(n):
+    d = 256 * 256 * 256
+    q = []
+    while d > 0:
+        m,n = divmod(n,d)
+        q.append(str(m))
+        d = d//256
+    return '.'.join(q)
 
-def find_imgs(url):
-    html = url_open(url).decode('utf-8')
-    img_addrs = []
+#数据库查询优化
+import  random,time
+ip_list=map(lambda x:x[1],random.sample(c,100))
+t1=time.time()
+# for i in ip_list:
+#     cur.execute("select *from ipdata where %s BETWEEN startip and endip",i)
+# t2=time.time()
+# print(t2-t1)
+#优化
+'''
+1.数据库中添加索引 startip endip
+2.SELECT *from ipdata where %s>=startip order by startip Desc LIMIT 1,i 试用于ip数据库连续
+'''
+# for i in ip_list:
+#     ret=cur.execute("SELECT *from ipdata where %s>=startip order by startip Desc LIMIT 1",i)
 
-    a = html.find('img id="maxImg1" src=')
-    b = html.find('.jpg', a , a+255)
-    if b !=-1:
-        img_addrs.append(html[a+22:b+4])
+'''
+3.100条语句一起执行
 
-    #print(img_addrs[0])
-    return img_addrs[0]
-
-
-def save_imgs(folder, img_address):
-    filename = img_address.split('/')[-1]
-    with open(filename, 'wb') as f:
-        img = url_open(img_address)
-        f.write(img)
-
-def download_mm(folder = 'jiandan', pages = 100):
-    # 生成文件夹
-    os.mkdir(folder)
-    # 改变工作目录
-    os.chdir(folder)
-
-    url = 'http://www.chunmm.com/meixiong/50213-'
-    #page_num = int(get_page(url))
-
-    for i in range(1,pages):
-        #page_num -= i
-        page_url = url + str(i) + '.html'
-
-        img_address = find_imgs(page_url)
-        save_imgs(folder, img_address)
-
-if __name__ == '__main__':
-    download_mm()
+'''
+sql_tmp="SELECT {0}. *from (select *from ipdata where %s>=startip order by startip Desc LIMIT 1){0}"
+l=list(ip_list)
+sql_list=[]
+for i in range(len(l)):
+    sql_list.append(sql_tmp.format("t"+str(i))%l[i])
+sql=' union all '.join(sql_list)
+cur.execute(sql)
+for i in cur.fetchall():
+    print(i)
